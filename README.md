fa2c57d7d92e88a817b70068a133340c801f2847
lazyou <1394139539@qq.com>
2019-03-06 09:43:29 +0800

    install init

---
+++ README.md
@@ -0,0 +1,8 @@
+## 简介
+* opencart 版本为: 1.5.6.4
+
+* `php -S localhost:8000`
+
+* 后台用户: `admin/111111`
+
+* 初始安装后 sql 备份: opencart_init.sql
---
+++ config-dist.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * NOTE: 此文件为模板案例. 重命名为 config.php 到当前目录下
+ */
+
+// HTTP
+define('HTTP_SERVER', 'http://localhost:8000/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:8000/');
+
+// DIR
+define('DIR_APPLICATION', '/mnt/d/Lazyou/opencart/catalog/');
+define('DIR_SYSTEM', '/mnt/d/Lazyou/opencart/system/');
+define('DIR_DATABASE', '/mnt/d/Lazyou/opencart/system/database/');
+define('DIR_LANGUAGE', '/mnt/d/Lazyou/opencart/catalog/language/');
+define('DIR_TEMPLATE', '/mnt/d/Lazyou/opencart/catalog/view/theme/');
+define('DIR_CONFIG', '/mnt/d/Lazyou/opencart/system/config/');
+define('DIR_IMAGE', '/mnt/d/Lazyou/opencart/image/');
+define('DIR_CACHE', '/mnt/d/Lazyou/opencart/system/cache/');
+define('DIR_DOWNLOAD', '/mnt/d/Lazyou/opencart/download/');
+define('DIR_LOGS', '/mnt/d/Lazyou/opencart/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', '127.0.0.1:33060');
+define('DB_USERNAME', 'homestead');
+define('DB_PASSWORD', 'secret');
+define('DB_DATABASE', 'opencart');
+define('DB_PREFIX', 'oc_');
+?>
